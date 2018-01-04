3a80ac9491e55f28cf03f0608b294519492b3e91
Salvazsahar <33119957+Salvazsahar@users.noreply.github.com>
2018-01-04 21:49:00 +0200

    Update Gomoku.py

--- Gomoku.py
+++ Gomoku.py
@@ -1,12 +1,59 @@
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.button import Button
+from kivy.uix.image import Image
+from kivy.uix.behaviors import ButtonBehavior
+from functools import partial
 import math
 
 
-def checkwin(count ,row, column, boarddiaright, boarddialeft, boardrow, boardcolumn, size):
+global gomokupvp
+def gomokupvp(r, c, count, turn, boarddiaright, boarddialeft, boardrow, boardcolumn, size):
+    color = 3-(turn & 1)
+    boardrow[r-1] += (1 << 2*(c-1))*color
+    # Column
+    boardcolumn[c - 1] += (1 << 2*(r - 1)) * color
+    # Right Diagonal
+    if r+c-1 > size:
+        boarddiaright[(r+c-1) - 1] += (1 << 2*(c-(r+c-1-size)-1)) * color
+    else:
+        boarddiaright[(r+c-1)-1] += (1 << 2*(c-1)) * color
+    # Left Diagonal
+    if c+size-r > size:
+        boarddialeft[(c+size-r) - 1] += (1 << 2*(r-1)) * color
+    else:
+        boarddialeft[(c+size-r)-1] += (1 << 2*(c-1)) * color
+    # End Placement
+    printboardrow(boardrow)
+    print boarddialeft, "aaaaa"
+    win = checkwin(count, r, c, boarddiaright, boarddialeft, boardrow, boardcolumn, size)
+    if win == 1:
+        print "player 1 has won"
+    elif win == -1:
+        print "player 2 has won"
+    return win
+
+
+def printboardrow(boardrow):
+    for i in boardrow:
+        for j in range(len(boardrow)):
+            a = i & 3
+            if a == 3:
+                print "o",
+            elif a == 2:
+                print "x",
+            else:
+                print ".",
+            i >>= 2
+        print
+
+
+def checkwin(count, row, column, boarddiaright, boarddialeft, boardrow, boardcolumn, size):
     mask = 85
     low = max(column - count, 0)
     high = min(size - 1, column + count - 2)
     num = boardrow[row - 1]
-    while (low <= high - count + 1):
+    while low <= high - count + 1:
         if (num >> 2*low) & mask*3 == mask*2:
             return 1
         elif (num >> 2 * low) & mask * 3 == mask * 3:
@@ -16,13 +63,13 @@ def checkwin(count ,row, column, boarddiaright, boarddialeft, boardrow, boardcol
     low = max(row - count, 0)
     high = min(size - 1, row + count - 2)
     num = boardcolumn[column - 1]
-    while (low <= high - count + 1):
+    while low <= high - count + 1:
         if (num >> 2 * low) & mask * 3 == mask * 2:
             return 1
         elif (num >> 2 * low) & mask * 3 == mask * 3:
             return -1
         low += 1
-    ##########1
+    # #########1
     if row + column - 1 > size:
         low = max((column - (row + column - 1 - size) - count), 0)
         high = min((column - (row + column - 1 - size) + count-2), row - 1 - 1)
@@ -30,7 +77,7 @@ def checkwin(count ,row, column, boarddiaright, boarddialeft, boardrow, boardcol
         low = max((column - count), 0)
         high = min((column + count-2), column - 1)
     num = boarddiaright[(row + column - 1) - 1]
-    while (low <= high - count + 1):
+    while low <= high - count + 1:
         if (num >> 2 * low) & mask * 3 == mask * 2:
             return 1
         elif (num >> 2 * low) & mask * 3 == mask * 3:
@@ -44,8 +91,7 @@ def checkwin(count ,row, column, boarddiaright, boarddialeft, boardrow, boardcol
         low = max((column - count), 0)
         high = min((column + count - 2), size - row + column - 1)
     num = boarddialeft[(column + size - row) - 1]
-    print num, low, high
-    while low <= high - (count-1):
+    while low <= high-count+1:
         if (num >> (2*low)) & (mask*3) == mask * 2:
             return 1
         elif (num >> (2 * low)) & (mask * 3) == mask * 3:
@@ -54,83 +100,51 @@ def checkwin(count ,row, column, boarddiaright, boarddialeft, boardrow, boardcol
     return 0
 
 
-def buildboardrowandcolumn(size):
-    boardrc = []
-    for i in xrange(size):
-        num = 0
-        for j in xrange(size):
-            num += 1<<2*j
-        boardrc.append(num)
-    return boardrc
+class Board(GridLayout, Image):
 
+    def __init__(self, **kwargs):
+        super(Board, self).__init__(**kwargs)
+        self.cols = 5
+        self.rows = 5
+        self.boardrow1 = [0, 0, 0, 0, 0]
+        self.boardcolumn1 = [0, 0, 0, 0, 0]
+        self.boarddiaright1 = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+        self.boarddialeft1 = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+        self.buttons = []
+        self.count = 4
+        self.turn = 1
+        self.win = 0
 
-def builboarddia(size):
-    boarddia = []
-    for i in xrange(1,size**2):
-        j = 0
-        num = 0
-        if i<=size:
-            while (j<=i):
-                num += 1<<2*j
-                j += 1
-        else:
-            while(j<= (size*2-i)):
-                num += 1<<2*j
-                j += 1
-        boarddia.append(num)
-    return boarddia
+        for i in range (self.rows):
+            for j in range (self.cols):
+                str1 = (str(i+1)+'-'+str(j+1))
+                self.buttons.append(Button(text=str1))
+                self.buttons[-1].bind(on_press=partial(callback, self, i+1, j+1))
+                self.add_widget(self.buttons[-1])
 
-
-def printboardrow(boardrow):
-    for i in boardrow:
-        for j in range (len(boardrow)):
-            a = i & 3
-            if a == 3:
-                print "o",
-            elif a == 2:
-                print "x",
-            else:
-                print ".",
-            i >>= 2
-        print
-
-
-
-def gomokupvp (count, turn, boarddiaright, boarddialeft, boardrow, boardcolumn, size):
-    color = 3-(turn&1)
-    r = int(raw_input("Enter the row you wish to place in, player " + str(turn&1) + " : "))
-    c = int(raw_input("Enter the column you wish to place in, player " + str(turn&1) + " : "))
-    boardrow[r-1] += (1<<2*(c-1)) * color
-    boardcolumn[c-1] += (1<<2*(r-1)) * color
-    if r+c-1 > size:
-        boarddiaright[(r + c - 1) - 1] += (1<<2*(c-(r+c-1-size)-1)) * color
+def callback(binstance, r, c, instance):
+    print "apple"
+    print type(c)
+    maskr = (1 << 2*(c-1))
+    print binstance.boardrow1, binstance.boardrow1[r-1], maskr, r, c
+    if binstance.boardrow1[r-1] & maskr*3 == maskr*2 or binstance.boardrow1[r-1] & maskr*3 == maskr*3:
+        print "Try Again"
     else:
-        boarddiaright[(r+c-1)-1] += (1<<2*(c-1)) * color
-    if c+size-r > size:
-        boarddialeft[(c + size - r) - 1] += (1<<2*(r-1)) * color
-    else:
-        boarddialeft[(c+size-r)-1] += (1<<2*(c-1)) * color
-    printboardrow(boardrow)
-    print boarddialeft
-    win = checkwin(count ,r, c, boarddiaright, boarddialeft, boardrow, boardcolumn, size)
-    print "b"
-    if win == 1:
-        print "player 1 has won"
-        return 1
-    elif win == -1:
-        print "player 2 has won"
-        return -1
-    return 0
+        win = gomokupvp(r, c, binstance.count, binstance.turn, binstance.boarddiaright1, binstance.boarddialeft1,
+                        binstance.boardrow1, binstance.boardcolumn1, 5)
+        print win
+        binstance.turn += 1
 
-boardrow = [0,0,0,0,0]
-boardcolumn = [0,0,0,0,0]
-boarddiaright = [0,0,0,0, 0 ,0,0,0,0]
-boarddialeft = [0,0,0,0, 0 ,0,0,0,0]
-size = 5
-count = 4
-turn = 1
-win = gomokupvp (count, turn, boarddiaright, boarddialeft, boardrow, boardcolumn, size)
-while win == 0:
-    turn += 1
-    win = gomokupvp(count, turn, boarddiaright, boarddialeft, boardrow, boardcolumn, size)
-    print "a"
+
+    # Move the checking part of the function to outside the function itself in order for the "trying again" to be possible
+
+
+class MyApp(App):
+
+    def build(self):
+        return Board()
+
+
+if __name__ == '__main__':
+    MyApp().run()
+

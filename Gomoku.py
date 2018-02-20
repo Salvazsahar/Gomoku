9c439b692d7faa43b6ba92585ebaf810e5c841d8
Salvazsahar <33119957+Salvazsahar@users.noreply.github.com>
2018-02-20 13:59:19 +0200

    Update Gomoku.py
    Finally finished the Player vs Computer, the only thing left is to connect it to the graphics.

--- Gomoku.py
+++ Gomoku.py
@@ -7,12 +7,12 @@ def score(count, row, column, boarddiaright, boarddialeft, boardrow, boardcolumn
     low = max(column - count + 1, 0)
     high = min(size - 1, column + count - 1)
     num = boardrow[row]
-    totscore += scoreHelpMe(low, high, num, color, count) + scoreAnnoyOpponent(low, high, num, color, count)
+    totscore += scoreHelpMe(low, high, num, color, count)*10 + scoreAnnoyOpponent(low, high, num, color, count)*9
     ##########
     low = max(row - count + 1, 0)
     high = min(size - 1, row + count - 1)
     num = boardcolumn[column]
-    totscore += scoreHelpMe(low, high, num, color, count) + scoreAnnoyOpponent(low, high, num, color, count)
+    totscore += scoreHelpMe(low, high, num, color, count)*10 + scoreAnnoyOpponent(low, high, num, color, count)*9
     # #########1
     if row + column + 1 > size:
         low = max((column - (row + column - size) - count), 0)
@@ -21,7 +21,7 @@ def score(count, row, column, boarddiaright, boarddialeft, boardrow, boardcolumn
         low = max((column - count + 1), 0)
         high = min((column + count - 1), row + column)
     num = boarddiaright[(row + column + 1) - 1]
-    totscore += scoreHelpMe(low, high, num, color, count) + scoreAnnoyOpponent(low, high, num, color, count)
+    totscore += scoreHelpMe(low, high, num, color, count)*10 + scoreAnnoyOpponent(low, high, num, color, count)*9
     ##########
     if column > row:
         low = max((row - count + 1), 0)
@@ -30,56 +30,103 @@ def score(count, row, column, boarddiaright, boarddialeft, boardrow, boardcolumn
         low = max((column - count + 1), 0)
         high = min((column + count - 1), size - row + column - 1)
     num = boarddialeft[(column + size - row) - 1]
-    totscore += scoreHelpMe(low, high, num, color, count) + scoreAnnoyOpponent(low, high, num, color, count)
+    totscore += scoreHelpMe(low, high, num, color, count)*10 + scoreAnnoyOpponent(low, high, num, color, count)*9
     return totscore
 
 
 def scoreHelpMe(low, high, num, color, count):
     countmax = 0
     while low <= high - count + 1:
-        i = 0
+        #### checking if there is an unblocked 4 or 5
+        j = 0
         counttimes = 0
         flag = 0
         num1 = num >> 2 * low
-        while i<5 and flag == 0:
-            if num1 & 3 == color:
-                counttimes+=1
-            if num1 & 3 == color^1:
-                flag == 1
-                countmax = 0
-            i += 1
+        while j < 6 and flag == 0:
+            if (num1 & 3 == color):
+                if j == 0 or j == 5:
+                    flag = 2
+                    counttimes = 0
+                else:
+                    counttimes += 1
+            if num1 & 3 == color^1 and j != 5:
+                flag = 1
+                counttimes = 0
+            j += 1
             num1 >>= 2
+        if flag == 0 and counttimes >= 3:
+            countmax = counttimes + 1
+        #### checkinf how many in a row there are
+        counttimes = 0
+        i = 0
+        num2 = num >> 2 * low
+        while i<5 and flag != 1 and countmax<4:
+            if num2 & 3 == color:
+                counttimes += 1
+            if num2 & 3 == color^1:
+                flag = 1
+                counttimes = 0
+            i += 1
+            num2 >>= 2
         if counttimes > countmax:
             countmax = counttimes
         low += 1
+    if countmax ==5:
+        if color == 2:
+            return (1 << (countmax) * 4)
+        else:
+            return (1 << (countmax*4)) * (-1)
     if countmax >= 2:
         if color == 2:
-            return (1 << (countmax-2)*3)
+            return (1 << ((countmax - 2)) * 3)
         else:
-            return (1 << (countmax-2)*3)*(-1)
+            return (1 << ((countmax - 2)) * 3)*(-1)
     return 0
 
 
 def scoreAnnoyOpponent(low, high, num, color, count):
     countmax = 0
     while low <= high - count + 1:
-        i = 0
+        j = 0
         counttimes = 0
         flag = 0
         num1 = num >> 2 * low
-        while i < 5 and flag == 0:
+        while j < 6 and flag == 0:
+            if (num1 & 3 == color^1):
+                if j == 0 or j == 5:
+                    flag = 2
+                    counttimes = 0
+                else:
+                    counttimes += 1
+            if num1 & 3 == color and j != 5:
+                flag = 1
+                counttimes = 0
+            j += 1
+            num1 >>= 2
+        if flag == 0 and counttimes >= 3:
+            countmax = counttimes + 1
+        i = 0
+        counttimes = 0
+        countmetimes = 0
+        flag = 0
+        num1 = num >> 2 * low
+        while i < 5 and flag != 1 and countmax<4:
             if num1 & 3 == color^1:
                 counttimes += 1
+            if num1 & 3 == color:
+                countmetimes += 1
             i += 1
             num1 >>= 2
+        if countmetimes > 1:
+            counttimes = 0
         if counttimes > countmax:
             countmax = counttimes
         low += 1
-    if countmax >= 2:
+    if countmax >= 1:
         if color == 2:
-            return (1 << (countmax - 2) * 3)
+            return (1 << ((countmax-1))*3)
         else:
-            return (1 << (countmax - 2) * 3) * (-1)
+            return (1 << ((countmax-1)*3))*(-1)
     return 0
 
 
@@ -257,7 +304,7 @@ def gomokupvp(count, turn, boarddiaright, boarddialeft, boardrow, boardcolumn, s
 def basic_mm_gomoku(count, color, boarddiaright, boarddialeft, boardrow, boardcolumn, boardpavailable, size, depth, remaining):
     bestrow = 0xdeadbeef
     bestcolumn = 0xdeadbeef
-    best = -10000 if color == 2 else 10000
+    best = -10000000000 if color == 2 else 10000000000
     places = choose_best_places(count, boarddiaright, boarddialeft, boardrow, boardcolumn, size, color, boardpavailable)
     # print "  " * depth, "begin d = ", depth, "color = ", color , "places before = ", places
     flag = 0
@@ -267,7 +314,7 @@ def basic_mm_gomoku(count, color, boarddiaright, boarddialeft, boardrow, boardco
         c = places[i][2]
         update(r, c, color, boarddiaright, boarddialeft, boardrow, boardcolumn, size)
         w = score(count, r, c, boarddiaright, boarddialeft, boardrow, boardcolumn, size, color)
-        if w < 1<<9 and w > (1<<9)*-1:
+        if w < (1<<20) and w > (1<<20)*-1:
             if remaining > 1 and depth<3:
                 place = 1 << (2 * c)
                 boardpavailable[r] ^= place
@@ -293,7 +340,8 @@ def mm_vs_player_gomoku(count, boarddiaright, boarddialeft, boardrow, boardcolum
     remaining = size*size
     while (turn <= size**2 and flag == 0):
         printboardrow(boardrow)
-        print "my board", boardrow
+        print "my board", boardrow, boardcolumn, boarddiaright, boarddialeft,
+        print "AAA", turn
         if turn & 1 == 1:
             color = 2
             r = int(raw_input("Enter the row you wish to place in, player " + str(turn & 1) + " : "))
@@ -301,18 +349,22 @@ def mm_vs_player_gomoku(count, boarddiaright, boarddialeft, boardrow, boardcolum
             place = 1 << 2*(c)
             while boardpavailable[r] & place == 0:
                 print "Try Again"
+                print "BBB", turn
                 r = int(raw_input("Enter the row you wish to place in, player " + str(turn & 1) + " : "))
                 c = int(raw_input("Enter the column you wish to place in, player " + str(turn & 1) + " : "))
                 place = 1 << 2 * (c)
             boardpavailable[r] ^= place
+            print "CCC", turn
             # print "EEEEEEEEEE", "dia right", boarddiaright[r + c], "dia left", boarddialeft[c + size - r - 1], "row", boardrow[r], "column", boardcolumn[c]
             update(r, c, color, boarddiaright, boarddialeft, boardrow, boardcolumn, size) # Updating the boards.
             win = checkwin(count, r, c, boarddiaright, boarddialeft, boardrow, boardcolumn, size)
             print "win p1 =", win
             if win == 1:
                 print "You have won"
-                flag == 1
+                flag = 1
+            print "DDD", flag
         else:
+            print "EEE", flag
             color = 3
             s, r, c = basic_mm_gomoku(count, color, boarddiaright, boarddialeft, boardrow, boardcolumn, boardpavailable, size,0, remaining)
             print "computer places here: \n", "Row: ", r, "Column: ",c
